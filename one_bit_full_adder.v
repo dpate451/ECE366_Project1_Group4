e6a6479af1f1fbdf5da77bd8babff54fe3f4fa01
dpate451 <dpate451@uic.edu>
2026-09-22 15:18:22 -0500

    Update one_bit_full_adder.v
    Implement 1-bit full adder behavioral logic

--- one_bit_full_adder.v
+++ one_bit_full_adder.v
@@ -1,1 +1,12 @@
+module one_bit_full_adder (A, B, Cin, S, Cout);
+    // Inputs defined by project spec
+    input A, B, Cin;
+    
+    // Outputs defined by project spec
+    output S, Cout;
 
+    // Behavioral logic for 1-bit addition
+    assign S = A ^ B ^ Cin;
+    assign Cout = (A & B) | (Cin & (A ^ B));
+
+endmodule
