4d176b2d82eb919d70dc17f82461d68176f52df3
dpate451 <dpate451@uic.edu>
2026-09-22 22:53:57 -0500

    Update one_bit_full_adder.v

--- one_bit_full_adder.v
+++ one_bit_full_adder.v
@@ -1,12 +1,11 @@
-module one_bit_full_adder (A, B, Cin, S, Cout);
-    // Inputs defined by project spec
-    input A, B, Cin;
-    
-    // Outputs defined by project spec
-    output S, Cout;
+module one_bit_full_adder(A, B, Cin, S, Cout);
 
-    // Behavioral logic for 1-bit addition
-    assign S = A ^ B ^ Cin;
-    assign Cout = (A & B) | (Cin & (A ^ B));
+input A, B, Cin;
+output reg S, Cout;
+
+always @(*) begin
+    S = A ^ B ^ Cin;
+    Cout = (A & B) | (A & Cin) | (B & Cin);
+end
 
 endmodule
