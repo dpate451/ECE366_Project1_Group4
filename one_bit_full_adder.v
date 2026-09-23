5eb853bebcb005cf9ef0058ac0584d3bf5a31324
dpate451 <dpate451@uic.edu>
2026-09-23 09:55:34 -0500

    Update one_bit_full_adder.v

--- one_bit_full_adder.v
+++ one_bit_full_adder.v
@@ -1,11 +1,9 @@
 module one_bit_full_adder(A, B, Cin, S, Cout);
 
 input A, B, Cin;
-output reg S, Cout;
+output S, Cout;
 
-always @(*) begin
-    S = A ^ B ^ Cin;
-    Cout = (A & B) | (A & Cin) | (B & Cin);
-end
+assign S = A ^ B ^ Cin;
+assign Cout = (A & B) | (A & Cin) | (B & Cin);
 
 endmodule
